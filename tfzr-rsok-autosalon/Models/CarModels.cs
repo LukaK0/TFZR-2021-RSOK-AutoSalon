92137b5d2193126f4cff1dae3a86185fc2ec6497
LukaK0 <lukakovacevic4@gmail.com>
2021-09-05 19:15:33 +0200

    CarModels model

---
+++ tfzr-rsok-autosalon/Models/CarModels.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace tfzr_rsok_autosalon.Models
+{
+    public class CarModels : BaseModel
+    {
+        public string Name { get; set; }
+        [ForeignKey("FK_Categorizes_CarModels")]
+        public virtual Categorizes Category { get; set; }
+
+        [ForeignKey("FK_Manufacturer_CarModels")]
+        public virtual Manufacturers Manufacturer { get; set; }
+
+        public override bool IsModelValid()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
