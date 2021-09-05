c12d1d655608644df39b6ca64455feb7de5d4cca
Levu <levule063@gmail.com>
2021-09-05 16:30:50 +0200

    Servis bazni model, viewmodel bazni

---
+++ tfzr-rsok-autosalon/Services/IServices/IService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tfzr_rsok_autosalon.Models;
+using tfzr_rsok_autosalon.Viewmodels.Base;
+
+namespace tfzr_rsok_autosalon.Services.IServices
+{
+    public interface IService<T, O> where T : BaseModel where O : IBaseViewModel<T>
+    {
+        O GetAll();
+        O Get(int id);
+        void Update(T model);
+        void Delete(int id);
+        void Add(T model);
+    }
+}
