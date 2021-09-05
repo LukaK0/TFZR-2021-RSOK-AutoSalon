52d073e1a3073e53709abbe56b94b09825b1611c
Levu <levule063@gmail.com>
2021-09-05 22:24:52 +0200

    Categories servis interfejs

---
+++ tfzr-rsok-autosalon/Viewmodels/CategorizesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tfzr_rsok_autosalon.Models;
+using tfzr_rsok_autosalon.Viewmodels.Base;
+
+namespace tfzr_rsok_autosalon.Viewmodels
+{
+    public class CategorizesViewModel : BaseViewModel<Categorizes>, IBaseViewModel<CategorizesViewModel>
+    {
+        public CategorizesViewModel(Categorizes model) : base(model)
+        {
+        }
+
+        public CategorizesViewModel CreateViewModel()
+        {
+            return this;
+        }
+    }
+}
