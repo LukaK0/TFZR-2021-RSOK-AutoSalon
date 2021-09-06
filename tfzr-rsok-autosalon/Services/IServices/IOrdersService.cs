5e6317b4194b02d98cd1881086d393e895ec18b0
Levu <levule063@gmail.com>
2021-09-06 19:46:27 +0200

    Orders servis interfejs

---
+++ tfzr-rsok-autosalon/Services/IServices/IOrdersService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using tfzr_rsok_autosalon.Models;
+using tfzr_rsok_autosalon.Viewmodels;
+using tfzr_rsok_autosalon.Viewmodels.Base;
+
+namespace tfzr_rsok_autosalon.Services.IServices
+{
+    public interface IOrdersService : IService<Orders, IBaseViewModel<Orders>>
+    {
+        IEnumerable<OrdersViewModel> GetAll();
+        SelectList GetUserForDropDown();
+        SelectList GetCarForDropDown();
+    }
+}
