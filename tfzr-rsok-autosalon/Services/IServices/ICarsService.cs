10528c5ad8ea2a76658d5d9ec5329b82570ada83
Levu <levule063@gmail.com>
2021-09-06 19:50:22 +0200

    CarModels i Cars servis interfejsi

---
+++ tfzr-rsok-autosalon/Services/IServices/ICarsService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using tfzr_rsok_autosalon.Models;
+using tfzr_rsok_autosalon.Viewmodels;
+using tfzr_rsok_autosalon.Viewmodels.Base;
+
+namespace tfzr_rsok_autosalon.Services.IServices
+{
+    public interface ICarsService : IService<Cars, IBaseViewModel<Cars>>
+    {
+        IEnumerable<CarsViewModel> GetAll();
+        SelectList GetCategoryForDropDown();
+        SelectList GetModelsForDropDown();
+        SelectList GetManufacturersForDropDown();
+    }
+}
---
+++ tfzr-rsok-autosalon/Viewmodels/CarsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tfzr_rsok_autosalon.Models;
+using tfzr_rsok_autosalon.Viewmodels.Base;
+
+namespace tfzr_rsok_autosalon.Viewmodels
+{
+    public class CarsViewModel : BaseViewModel<Cars>, IBaseViewModel<CarsViewModel>
+    {
+        public CarsViewModel(Cars model) : base(model)
+        {
+        }
+
+        public CarsViewModel CreateViewModel()
+        {
+            return this;
+        }
+    }
+}
