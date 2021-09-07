bdf2d14d31baa365c58160b7f5b3f696df84dff3
Vukasin Vukovic <levule063@gmail.com>
2021-09-07 10:54:46 +0200

    Orders repo interfejs

---
+++ tfzr-rsok-autosalon/Data/Repository/IRepository/IOrdersRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tfzr_rsok_autosalon.Models;
+
+namespace tfzr_rsok_autosalon.Data.Repository.IRepository
+{
+    public interface IOrdersRepository : IRepository<Orders>
+    {
+    }
+}
